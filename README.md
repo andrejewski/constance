db09436484072dea3e43ee28809174074a47449f
Chris Andrejewski <christopher.andrejewski@gmail.com>
2014-11-16 12:52:57 -0500

    Real Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-constance
+Constance
 =========
 
-Constants with prefix and postfix options
+Create constants with ease. Inspired by React's `keyMirror` module, Constance accepts an object and mirrors the keys onto their values. Constance also allows you to prefix and postfix constants, either to provide proper namespacing or cut down on the verbosity of writing out constants. Constance works with arrays too.
+
+```js
+var constance = require('constance');
+
+var methods = ['save', 'update', 'validate'];
+
+// prefixing
+var hooks = constance('pre_', methods); 
+/* => {
+	save: 'pre_save',
+	update: 'pre_update',
+	validate: 'pre_validate'	
+} */
+
+// postfixing
+var actions = constance(methods, '_action');
+/* => {
+	save: 'save_action',
+	update: 'update_action',
+	validate: 'validate_action'	
+} */
+```
+
+### Use Case
+
+I created this module while working with Facebook's Flux architecture. `keyMirror` does an okay job, but without the option to prefix and postfix, action types were getting verbose and inconsistent.
---
+++ test/constance.js
@@ -0,0 +1,62 @@
+
+var assert = require('assert'),
+	constance = require('../');
+
+describe('constance()', function() {
+	it('should key mirror for a plain object', function() {
+		var objConsts = constance({
+			'key1': null,
+			'key2': null
+		});
+		var arrConsts = constance(['key1', 'key2']);
+		var outConsts = {
+			'key1': 'key1',
+			'key2': 'key2'
+		};
+		assert.deepEqual(objConsts, outConsts);
+		assert.deepEqual(arrConsts, outConsts);
+	});
+	it('should key mirror with prefix for prefix and object', function() {
+		var prefix = "pre_";
+		var objConsts = constance(prefix, {
+			'key1': null,
+			'key2': null
+		});
+		var arrConsts = constance(prefix, ['key1', 'key2']);
+		var outConsts = {
+			'key1': prefix+'key1',
+			'key2': prefix+'key2'
+		};
+		assert.deepEqual(objConsts, outConsts);
+		assert.deepEqual(arrConsts, outConsts);
+	});
+	it('should key mirror with postfix for object and postfix', function() {
+		var postfix = "_post";
+		var objConsts = constance({
+			'key1': null,
+			'key2': null
+		}, postfix);
+		var arrConsts = constance(['key1', 'key2'], postfix);
+		var outConsts = {
+			'key1': 'key1'+postfix,
+			'key2': 'key2'+postfix
+		};
+		assert.deepEqual(objConsts, outConsts);
+		assert.deepEqual(arrConsts, outConsts);
+	});
+	it('should key mirror with prefix and postfix for prefix, object, and postfix', function() {
+		var prefix = "pre_";
+		var postfix = "_post";
+		var objConsts = constance(prefix, {
+			'key1': null,
+			'key2': null
+		}, postfix);
+		var arrConsts = constance(prefix, ['key1', 'key2'], postfix);
+		var outConsts = {
+			'key1': prefix+'key1'+postfix,
+			'key2': prefix+'key2'+postfix
+		};
+		assert.deepEqual(objConsts, outConsts);
+		assert.deepEqual(arrConsts, outConsts);
+	});
+});
